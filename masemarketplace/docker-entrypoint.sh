b36a158c5745fe0fb52f77585ee7b773c1a364c0
MedAmine1990 <med.amine.souissi@gmail.com>
2022-05-24 20:09:25 +0200

    Pull from github master branch at startup

--- masemarketplace/docker-entrypoint.sh
+++ masemarketplace/docker-entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+cd /usr/src/masemarketplace_app/masemarketplace/
+
+git fetch --all
+
+git reset --hard origin/master
+
 set -e
 /usr/src/masemarketplace_app/masemarketplace/manage.py runserver 0.0.0.0:8000
 
