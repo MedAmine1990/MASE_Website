43cedf730bd3e7183803fae840d437b07289c629
MedAmine1990 <med.amine.souissi@gmail.com>
2022-07-04 17:27:20 +0000

    Login features: jwt login implementation.

---
+++ masemarketplace/backend_userManagement/loginjwtservice.py
@@ -0,0 +1,22 @@
+from rest_framework_jwt.settings import api_settings
+from rest_framework_jwt.compat import set_cookie_with_token
+from .models import user
+
+
+
+
+
+def jwt_login(*, response: HttpResponse, user: User) -> HttpResponse:
+    jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+    payload = jwt_payload_handler(user)
+    token = jwt_encode_handler(payload)
+
+    #if api_settings.JWT_AUTH_COOKIE:
+        # Reference: https://github.com/Styria-Digital/django-rest-framework-jwt/blob/master/src/rest_framework_jwt/compat.py#L43
+    #    set_cookie_with_token(response, api_settings.JWT_AUTH_COOKIE, token)
+
+    #user_record_login(user=user)
+
+    return token
--- masemarketplace/googleAuth/services.py
+++ masemarketplace/googleAuth/services.py
@@ -17,22 +17,6 @@ GOOGLE_ACCESS_TOKEN_OBTAIN_URL = 'https://oauth2.googleapis.com/token'
 GOOGLE_USER_INFO_URL = 'https://www.googleapis.com/oauth2/v3/userinfo'
 
 
-#def jwt_login(*, response: HttpResponse, user: User) -> HttpResponse:
-#    jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-#    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-
-#    payload = jwt_payload_handler(user)
-#    token = jwt_encode_handler(payload)
-
-#    if api_settings.JWT_AUTH_COOKIE:
-        # Reference: https://github.com/Styria-Digital/django-rest-framework-jwt/blob/master/src/rest_framework_jwt/compat.py#L43
-#        set_cookie_with_token(response, api_settings.JWT_AUTH_COOKIE, token)
-
-#    user_record_login(user=user)
-
-#    return response
-
-
 def google_validate_id_token(*, id_token: str) -> bool:
     # Reference: https://developers.google.com/identity/sign-in/web/backend-auth#verify-the-integrity-of-the-id-token
     response = requests.get(
