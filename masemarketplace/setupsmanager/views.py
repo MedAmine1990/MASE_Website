e0b675763f73df3745ae290843e7cf53db14546a
MedAmine1990 <med.amine.souissi@gmail.com>
2024-01-04 20:32:53 +0000

    continued

--- masemarketplace/setupsmanager/views.py
+++ masemarketplace/setupsmanager/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import *
 # Create your views here.
 
@@ -25,7 +26,7 @@ class CreateSetup(APIView):
             _popometerLink=request.data["popometerLink"]
             _published=request.data["published"]
         except Exception as error:
-            print("request error:", str(error))
+            return Response({"request error": str(error)})
         try:
             _carsetup=carsetup(car=_car
             track=_track
@@ -37,12 +38,22 @@ class CreateSetup(APIView):
             popometerLink=_popometerLink
             published=_published)
             _carsetup.save()
+            return Response({"request success": "Setup information saved."})
         except Exception as error:
-            print("carsetup save error:", str(error))
+            return Response({"request error": str(error)})
         
 
 class UpdateSetup(APIView):
     def post(self, request, format=None):
+        _car=None
+        _track=None
+        _qualilaptime=None
+        _racelaptime=None
+        _thumbnailName=None
+        _interests=None
+        _setsbaseLink=None
+        _popometerLink=None
+        _published=None
         try:
             _car=request.data["car"]
             _track=request.data["track"]
@@ -54,22 +65,42 @@ class UpdateSetup(APIView):
             _popometerLink=request.data["popometerLink"]
             _published=request.data["published"]
         except Exception as error:
-            print("request error:", str(error))
+            return Response({"request error": str(error)})
+        try:
+            check_cartrack_combo=carsetup.objects.filter(car=_car, track=_track)
+            if not check_cartrack_combo:
+                return Response({"request error": "This car track combo does not exist."})
+            else:
+                carsetup.objects.filter(car=_car, track=_track).update(
+                    car=_car,
+                    track=_track,
+                    qualilaptime=_qualilaptime,
+                    racelaptime=_racelaptime,
+                    thumbnailName=_thumbnailName,
+                    interests=_interests,
+                    setsbaseLink=_setsbaseLink,
+                    popometerLink=_popometerLink,
+                    published=_published
+                )
+                return Response({"request success": "Setup information updated."})
+        except Exception as error:
+            return Response({"request error": str(error)})
 
 class DeleteSetup(APIView):
     def post(self, request, format=None):
         try:
             _car=request.data["car"]
             _track=request.data["track"]
-            _qualilaptime=request.data["qualilaptime"]
-            _racelaptime=request.data["racelaptime"]
-            _thumbnailName=request.data["thumbnailName"]
-            _interests=request.data["interests"]
-            _setsbaseLink=request.data["setsbaseLink"]
-            _popometerLink=request.data["popometerLink"]
-            _published=request.data["published"]
         except Exception as error:
-            print("request error:", str(error))
+            return Response({"request error": str(error)})
+        try:
+            check_cartrack_combo=carsetup.objects.filter(car=_car, track=_track)
+            if not check_cartrack_combo:
+                return Response({"request error": "This car track combo does not exist."})
+            else:
+                carsetup.objects.filter(car=_car, track=_track).delete()
+        except Exception as error:
+            return Response({"request error": str(error)})
 
 class GetSetup(APIView):
     def get(self, request, format=None):
